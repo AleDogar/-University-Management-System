085ea0be3f1fce136be33f0d2dbb4a084c471562
Alesia <dogar_alesia@yahoo.com>
2025-10-28 21:31:23 +0200

    Staff

--- University-Management-System/src/main/java/com/example/University/Management/System/model/Staff.java
+++ University-Management-System/src/main/java/com/example/University/Management/System/model/Staff.java
@@ -1,4 +1,48 @@
 package com.example.University.Management.System.model;
 
-public class Staff {
+import java.util.List;
+
+public abstract class Staff {
+    private String staffID;
+    private String staffName;
+    private List<TeachingAssignment> assignments;
+
+    public Staff(List<TeachingAssignment> assignments, String staffName, String staffID) {
+        this.assignments = assignments;
+        this.staffName = staffName;
+        this.staffID = staffID;
+    }
+
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getStaffName() {
+        return staffName;
+    }
+
+    public void setStaffName(String staffName) {
+        this.staffName = staffName;
+    }
+
+    public List<TeachingAssignment> getAssignments() {
+        return assignments;
+    }
+
+    public void setAssignments(List<TeachingAssignment> assignments) {
+        this.assignments = assignments;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" +
+                "staffID='" + staffID + '\'' +
+                ", staffName='" + staffName + '\'' +
+                ", assignments=" + assignments +
+                '}';
+    }
 }
