a66bd0574c7c11400d1c9b9d64401f8152d25185
Alesia <dogar_alesia@yahoo.com>
2025-10-28 20:15:30 +0200

    university

--- University-Management-System/src/main/java/com/example/University/Management/System/model/University.java
+++ University-Management-System/src/main/java/com/example/University/Management/System/model/University.java
@@ -1,4 +1,74 @@
 package com.example.University.Management.System.model;
 
+import com.example.University.Management.System.model.Department;
+import com.example.University.Management.System.model.Room;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class University {
+    private String id;
+    private String name;
+    private String city;
+    private List<Department> departments;
+    private List<Room> rooms;
+
+    public University(String id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.departments = new ArrayList<>();
+        this.rooms = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<Department> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(List<Department> departments) {
+        this.departments = departments;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    @Override
+    public String toString() {
+        return "University{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", departments=" + departments +
+                ", rooms=" + rooms +
+                '}';
+    }
 }
