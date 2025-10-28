734367a7a6a5ad30f1ea22c25736920b3a0188c9
Alesia <dogar_alesia@yahoo.com>
2025-10-28 21:14:46 +0200

    teacher

--- University-Management-System/src/main/java/com/example/University/Management/System/model/Teacher.java
+++ University-Management-System/src/main/java/com/example/University/Management/System/model/Teacher.java
@@ -1,4 +1,35 @@
 package com.example.University.Management.System.model;
 
 public class Teacher {
+    private String title;
+    private String departmentID;
+
+    public Teacher(String title, String departmentID) {
+        this.title = title;
+        this.departmentID = departmentID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDepartmentID() {
+        return departmentID;
+    }
+
+    public void setDepartmentID(String departmentID) {
+        this.departmentID = departmentID;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "title='" + title + '\'' +
+                ", departmentID='" + departmentID + '\'' +
+                '}';
+    }
 }
