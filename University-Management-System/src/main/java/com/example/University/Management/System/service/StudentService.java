edcf0be7bf051b82efe2a990b3ab31cc9d80ce4f
Alesia <dogar_alesia@yahoo.com>
2025-10-28 23:37:38 +0200

    departament

--- University-Management-System/src/main/java/com/example/University/Management/System/service/StudentService.java
+++ University-Management-System/src/main/java/com/example/University/Management/System/service/StudentService.java
@@ -1,4 +1,36 @@
 package com.example.University.Management.System.service;
 
+import com.example.University.Management.System.model.Student;
+import com.example.University.Management.System.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public void addStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    public void removeStudent(String studentId) {
+        Student student = studentRepository.findById(studentId);
+        if (student != null) {
+            studentRepository.delete(student);
+        }
+    }
+
+    public Student getStudentById(String id) {
+        return studentRepository.findById(id);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
