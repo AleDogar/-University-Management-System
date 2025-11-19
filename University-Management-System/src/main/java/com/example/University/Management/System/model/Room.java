f96ad8905cd3c71b731c3be1f94ed0e90849b130
mihai <mihai2005.dreghiciu@gmail.com>
2025-11-19 03:14:56 +0200

    rooms gata

--- University-Management-System/src/main/java/com/example/University/Management/System/model/Room.java
+++ University-Management-System/src/main/java/com/example/University/Management/System/model/Room.java
@@ -4,17 +4,17 @@ import java.util.List;
 
 public class Room {
     private String roomID;
-    private double capacity;
     private String number;
+    private double capacity;
     private List<Course> courses;
 
-    public Room(String roomID, double capacity, String number, List<Course> courses) {
+    // Constructor corect: number înaintea capacity
+    public Room(String roomID, String number, double capacity, List<Course> courses) {
         this.roomID = roomID;
-        this.capacity = capacity;
         this.number = number;
+        this.capacity = capacity;
         this.courses = courses;
     }
-
 
     public String getRoomID() {
         return roomID;
@@ -24,20 +24,20 @@ public class Room {
         this.roomID = roomID;
     }
 
-    public double getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(double capacity) {
-        this.capacity = capacity;
-    }
-
     public String getNumber() {
         return number;
     }
 
     public void setNumber(String number) {
         this.number = number;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
     }
 
     public List<Course> getCourses() {
@@ -52,8 +52,8 @@ public class Room {
     public String toString() {
         return "Room{" +
                 "roomID='" + roomID + '\'' +
-                ", capacity=" + capacity +
                 ", number='" + number + '\'' +
+                ", capacity=" + capacity +
                 ", courses=" + courses +
                 '}';
     }
--- University-Management-System/src/main/java/com/example/University/Management/System/repository/RoomRepository.java
+++ University-Management-System/src/main/java/com/example/University/Management/System/repository/RoomRepository.java
@@ -1,16 +1,27 @@
 package com.example.University.Management.System.repository;
 
 import com.example.University.Management.System.model.Room;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RoomRepository extends InMemoryRepository<Room> {
+@Profile("infile")
+public class RoomRepository extends InFileRepository<Room> {
+
+    public RoomRepository(ObjectMapper mapper,
+                          @Value("${app.data-folder}") String folder) {
+        super(mapper, folder, "rooms.json");
+    }
 
     @Override
     protected String getId(Room entity) {
-        return entity.getRoomID(); }
+        return entity.getRoomID();
+    }
 
     @Override
     protected void setId(Room entity, String id) {
-        entity.setRoomID(id); }
+        entity.setRoomID(id);
+    }
 }
