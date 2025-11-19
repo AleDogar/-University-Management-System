9d71d62b942740a5c913797a5ce93267d4f08c93
mihai <mihai2005.dreghiciu@gmail.com>
2025-11-19 02:58:02 +0200

    departments gata

--- University-Management-System/src/main/java/com/example/University/Management/System/model/Enrollment.java
+++ University-Management-System/src/main/java/com/example/University/Management/System/model/Enrollment.java
@@ -1,46 +1,48 @@
-    package com.example.University.Management.System.model;
+package com.example.University.Management.System.model;
 
-    public class Enrollment {
-        private String enrollmentID;
-        private String courseID;
-        private ClassGrade grade;
+public class Enrollment {
+    private String enrollmentID;
+    private String courseID;
+    private ClassGrade grade;
 
-        public Enrollment(String enrollmentID, String courseID, ClassGrade grade) {
-            this.enrollmentID = enrollmentID;
-            this.courseID = courseID;
-            this.grade = grade;
-        }
+    public Enrollment() {}
 
-        public String getEnrollmentID() {
-            return enrollmentID;
-        }
-
-        public void setEnrollmentID(String enrollmentID) {
-            this.enrollmentID = enrollmentID;
-        }
-
-        public String getCourseID() {
-            return courseID;
-        }
-
-        public void setCourseID(String courseID) {
-            this.courseID = courseID;
-        }
-
-        public ClassGrade getGrade() {
-            return grade;
-        }
-
-        public void setGrade(ClassGrade grade) {
-            this.grade = grade;
-        }
-
-        @Override
-        public String toString() {
-            return "Enrollment{" +
-                    "enrollmentID='" + enrollmentID + '\'' +
-                    ", courseID='" + courseID + '\'' +
-                    ", grade=" + grade +
-                    '}';
-        }
+    public Enrollment(String enrollmentID, String courseID, ClassGrade grade) {
+        this.enrollmentID = enrollmentID;
+        this.courseID = courseID;
+        this.grade = grade;
     }
+
+    public String getEnrollmentID() {
+        return enrollmentID;
+    }
+
+    public void setEnrollmentID(String enrollmentID) {
+        this.enrollmentID = enrollmentID;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public ClassGrade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(ClassGrade grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Enrollment{" +
+                "enrollmentID='" + enrollmentID + '\'' +
+                ", courseID='" + courseID + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}
--- University-Management-System/src/main/java/com/example/University/Management/System/service/EnrollmentService.java
+++ University-Management-System/src/main/java/com/example/University/Management/System/service/EnrollmentService.java
@@ -5,25 +5,34 @@ import com.example.University.Management.System.repository.EnrollmentRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EnrollmentService {
 
-    private final EnrollmentRepository enrollmentRepository;
+    private final EnrollmentRepository repository;
 
-    public EnrollmentService(EnrollmentRepository enrollmentRepository) {
-        this.enrollmentRepository = enrollmentRepository;
+    public EnrollmentService(EnrollmentRepository repository) {
+        this.repository = repository;
     }
 
     public List<Enrollment> getAllEnrollments() {
-        return enrollmentRepository.findAll();
+        return repository.findAll();
     }
 
-    public Enrollment addEnrollment(Enrollment enrollment) {
-        return enrollmentRepository.save(enrollment);
+    public Enrollment getEnrollmentById(String id) {
+        return repository.findById(id);
+    }
+
+    public Enrollment saveEnrollment(Enrollment e) {
+        if (e.getEnrollmentID() == null || e.getEnrollmentID().isEmpty()) {
+            e.setEnrollmentID(UUID.randomUUID().toString());
+        }
+        repository.save(e);
+        return e;
     }
 
     public void removeEnrollment(String id) {
-        enrollmentRepository.deleteById(id);
-}
+        repository.deleteById(id);
+    }
 }
